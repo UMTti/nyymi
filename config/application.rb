4dbd5d68b525bf962855ab3b3bec2945ddfec7aa
Pihla Toivanen <toivanenpihla@gmail.com>
2017-03-12 16:48:58 +0200

    fix

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Nyymi
     # Do not swallow errors in after_commit/after_rollback callbacks.
 
     I18n.enforce_available_locales = true
-    config.i18n.default_locale = :'en'
+    config.i18n.locale = :'en'
 
     config.active_record.raise_in_transactional_callbacks = true
 
